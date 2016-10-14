ef0a5e00c00e6c245d4ad27964ad330e2d037bc1
Cedd Burge <cedd.burge@res-ltd.com>
2016-10-14 19:16:47 +0100

    Start a sample test

---
+++ SampleSystemUnderTest/Cargo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSystemUnderTest
+{
+    public class Cargo : ICargo
+    {
+
+        public Cargo(string origin, string destination, IItinerary itinerary)
+        {
+            Contract.Requires(itinerary != null);
+
+            Origin = origin;
+            Destination = destination;
+            Itinerary = itinerary;
+        }
+
+        public string Destination { get; }
+        public string Origin { get; }
+        public IItinerary Itinerary { get; }
+    }
+}
---
+++ SampleSystemUnderTest/RoutingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSystemUnderTest
+{
+    public class RoutingService
+    {
+        public string RerouteFrom { get; }
+        public string RerouteTo { get; }
+        public ICargo Cargo { get; }
+
+        public RoutingService(string rerouteFrom , string rerouteTo, ICargo cargo)
+        {
+            Contract.Requires(cargo != null);
+
+            Cargo = cargo;
+            RerouteFrom = rerouteFrom;
+            RerouteTo = rerouteTo;
+        }
+
+        public ICargo Reroute()
+        {
+            return Cargo;
+        }
+
+    }
+}
