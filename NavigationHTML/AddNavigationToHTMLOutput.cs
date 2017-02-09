d4b5a459abf0b7b9a7b9d4d47c9078dee080e35a
Cyril Gandon <cyril.gandon@res-group.com>
2017-02-09 09:54:38 +0100

    Split DirectoryToIndex in its own file

--- NavigationHTML/AddNavigationToHTMLOutput.cs
+++ NavigationHTML/AddNavigationToHTMLOutput.cs
@@ -8,36 +8,30 @@ namespace RES.Specification.NavigationHTML
 {
     public class AddNavigationToHTMLOutput
     {
-        protected string _rootDirectory;
-        protected IFileSystem _fileSystem;
-        protected Queue<DirectoryToIndex> _directories;
-        protected INavigationHTMLFormatter _formatter;
-
-        protected struct DirectoryToIndex
-        {
-            public string FullDirectory;
-            public string ParentDirectory;
-            public string Description;
-            public int NestDepth;
-        }
-
+        private Queue<DirectoryToIndex> _directories = new Queue<DirectoryToIndex>();
+        public IFileSystem FileSystem { get; }
+        public INavigationHTMLFormatter Formatter { get; }
 
         public AddNavigationToHTMLOutput(IFileSystem fileSystem, INavigationHTMLFormatter formatter)
         {
             if (fileSystem == null) throw new ArgumentNullException("fileSystem");
             if (formatter == null) throw new ArgumentNullException("formatter");
 
-            _fileSystem = fileSystem;
-            _formatter = formatter;
-
-            _directories = new Queue<DirectoryToIndex>();
+            FileSystem = fileSystem;
+            Formatter = formatter;
         }
 
         public void CreateIndexHtmlFiles(string rootDirectory, string rootName)
         {
             if (string.IsNullOrEmpty(rootDirectory)) throw new ArgumentNullException("rootDirectory");
 
-            _directories.Enqueue(new DirectoryToIndex() { FullDirectory = rootDirectory, ParentDirectory = null, Description = rootName, NestDepth = 0 });
+            var directoryToIndex = new DirectoryToIndex(
+                fullDirectory: rootDirectory,
+                parentDirectory: null,
+                description: rootName,
+                nestDepth: 0);
+
+            _directories.Enqueue(directoryToIndex);
 
             while (_directories.Any())
             {
@@ -45,62 +39,60 @@ namespace RES.Specification.NavigationHTML
             }
         }
 
-        protected void AddIndex(DirectoryToIndex directoryToIndex)
+        private void AddIndex(DirectoryToIndex directoryToIndex)
         {
-            _formatter.StartIndex(directoryToIndex.Description, directoryToIndex.NestDepth);
+            Formatter.StartIndex(directoryToIndex.Description, directoryToIndex.NestDepth);
 
             if (string.IsNullOrEmpty(directoryToIndex.ParentDirectory) == false)
             {
-                _formatter.StartParent();
-                _formatter.AddLink(Index(".."), GetLastDirectoryName(directoryToIndex.ParentDirectory), "parentLink");
-                _formatter.EndParent();
+                Formatter.StartParent();
+                Formatter.AddLink(Index(".."), GetLastDirectoryName(directoryToIndex.ParentDirectory), "parentLink");
+                Formatter.EndParent();
             }
 
-            _formatter.StartTests();
-            foreach (var filename in _fileSystem.Directory.GetFiles(directoryToIndex.FullDirectory, "*.html"))
+            Formatter.StartTests();
+            foreach (var filename in FileSystem.Directory.GetFiles(directoryToIndex.FullDirectory, "*.html"))
             {
-                if (_fileSystem.Path.GetFileName(filename).ToLower() != "index.html")
+                if (FileSystem.Path.GetFileName(filename).ToLower() != "index.html")
                 {
-                    _formatter.AddLink(_fileSystem.Path.GetFileName(filename), _fileSystem.Path.GetFileNameWithoutExtension(filename), "testLink");
+                    Formatter.AddLink(FileSystem.Path.GetFileName(filename), FileSystem.Path.GetFileNameWithoutExtension(filename), "testLink");
                 }
             }
-            _formatter.EndTests();
+            Formatter.EndTests();
 
-            var subSirectories = _fileSystem.Directory.GetDirectories(directoryToIndex.FullDirectory);
+            var subSirectories = FileSystem.Directory.GetDirectories(directoryToIndex.FullDirectory);
             if (subSirectories.Any())
             {
-                _formatter.StartChildren();
-                foreach (var fullDirectory in _fileSystem.Directory.GetDirectories(directoryToIndex.FullDirectory))
+                Formatter.StartChildren();
+                foreach (var fullDirectory in FileSystem.Directory.GetDirectories(directoryToIndex.FullDirectory))
                 {
-                    if (fullDirectory.TrimEnd(_fileSystem.Path.DirectorySeparatorChar).ToLower() != directoryToIndex.FullDirectory.TrimEnd(_fileSystem.Path.DirectorySeparatorChar).ToLower())
+                    if (fullDirectory.TrimEnd(FileSystem.Path.DirectorySeparatorChar).ToLower() != directoryToIndex.FullDirectory.TrimEnd(FileSystem.Path.DirectorySeparatorChar).ToLower())
                     {
                         var subDirectory = GetLastDirectoryName(fullDirectory);
-                        _formatter.AddLink(Index(subDirectory), subDirectory, "childLink");
-                        _directories.Enqueue(
-                            new DirectoryToIndex()
-                            {
-                                FullDirectory = fullDirectory,
-                                ParentDirectory = directoryToIndex.FullDirectory,
-                                Description = subDirectory,
-                                NestDepth = directoryToIndex.NestDepth + 1
-                            });
+                        Formatter.AddLink(Index(subDirectory), subDirectory, "childLink");
+
+                        _directories.Enqueue(new DirectoryToIndex(
+                            fullDirectory,
+                            parentDirectory: directoryToIndex.FullDirectory,
+                            description: subDirectory,
+                            nestDepth: directoryToIndex.NestDepth + 1));
                     }
                 }
-                _formatter.EndChildren();
+                Formatter.EndChildren();
             }
-            _formatter.EndIndex();
+            Formatter.EndIndex();
 
             SaveIndex(Index(directoryToIndex.FullDirectory));
         }
 
         private string GetLastDirectoryName(string parentDirectory)
         {
-            return _fileSystem.Path.GetFileName(parentDirectory.TrimEnd(_fileSystem.Path.DirectorySeparatorChar));
+            return FileSystem.Path.GetFileName(parentDirectory.TrimEnd(FileSystem.Path.DirectorySeparatorChar));
         }
 
         private void SaveIndex(string filename)
         {
-            _fileSystem.File.WriteAllText(filename, _formatter.HTML());
+            FileSystem.File.WriteAllText(filename, Formatter.HTML());
         }
 
         private string Index(string directory)
--- NavigationHTML/NavigationHTMLFormatter.cs
+++ NavigationHTML/NavigationHTMLFormatter.cs
@@ -7,91 +7,86 @@ namespace RES.Specification.NavigationHTML
 {
     public class NavigationHTMLFormatter : INavigationHTMLFormatter
     {
-        protected StringBuilder _indexHTML;
+        private StringBuilder Builder { get; } = new StringBuilder();
 
-        public NavigationHTMLFormatter()
-        {
-            _indexHTML = new StringBuilder();
-        }
-
-        public string HTML()
-        {
-            return _indexHTML.ToString();
-        }
+        public string HTML() => Builder.ToString();
 
         public void StartIndex(string description, int nestDepth)
         {
-            _indexHTML.Clear();
-            _indexHTML.AppendLine(@"<!DOCTYPE html PUBLIC ""-//W3C//DTD XHTML 1.0 Strict//EN\""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"">");
-            _indexHTML.AppendLine(@"<html>");
-            _indexHTML.AppendLine(@"<head>");
-            _indexHTML.AppendLine(@"<title>");
-            _indexHTML.AppendLine(description);
-            _indexHTML.AppendLine(@"</title>");
-            _indexHTML.AppendLine(@"</head>");
-            _indexHTML.Append(@"<link rel='stylesheet' href='"); // stylesheet should be in the directory above the root directory for tests
-            for (int i = 0; i <= nestDepth; i++) _indexHTML.Append(@"..\");
-            _indexHTML.AppendLine(@"SpecificationIndex.css'>");
-            _indexHTML.AppendLine(@"<body>");
-            _indexHTML.AppendLine(@"<h1>");
-            _indexHTML.AppendLine(description);
-            _indexHTML.AppendLine(@"</h1>");
+            Builder.Clear();
+            Builder.AppendLine(@"<!DOCTYPE html PUBLIC ""-//W3C//DTD XHTML 1.0 Strict//EN\""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"">");
+            Builder.AppendLine(@"<html>");
+            Builder.AppendLine(@"<head>");
+            Builder.AppendLine(@"<title>");
+            Builder.AppendLine(description);
+            Builder.AppendLine(@"</title>");
+            Builder.AppendLine(@"</head>");
+            Builder.Append(@"<link rel='stylesheet' href='"); // stylesheet should be in the directory above the root directory for tests
+            for (int i = 0; i <= nestDepth; i++)
+            {
+                Builder.Append(@"..\");
+            }
+            Builder.AppendLine(@"SpecificationIndex.css'>");
+            Builder.AppendLine(@"<body>");
+            Builder.AppendLine(@"<h1>");
+            Builder.AppendLine(description);
+            Builder.AppendLine(@"</h1>");
         }
 
         public void StartParent()
         {
-            _indexHTML.AppendLine(@"<div class='parent'>");
-            _indexHTML.AppendLine("<h2>Parent Tests</h2>");
-            _indexHTML.AppendLine(@"<ul class='parentLinks'>");
+            Builder.AppendLine(@"<div class='parent'>");
+            Builder.AppendLine("<h2>Parent Tests</h2>");
+            Builder.AppendLine(@"<ul class='parentLinks'>");
         }
 
         public void EndParent()
         {
-            _indexHTML.AppendLine(@"</ul>");
-            _indexHTML.AppendLine("</div>");
+            Builder.AppendLine(@"</ul>");
+            Builder.AppendLine("</div>");
         }
 
         public void StartTests()
         {
-            _indexHTML.AppendLine(@"<div class='tests'>");
-            _indexHTML.AppendLine("<h2>Tests</h2>");
-            _indexHTML.AppendLine(@"<ul class='testLinks'>");
+            Builder.AppendLine(@"<div class='tests'>");
+            Builder.AppendLine("<h2>Tests</h2>");
+            Builder.AppendLine(@"<ul class='testLinks'>");
         }
 
         public void EndTests()
         {
-            _indexHTML.AppendLine(@"</ul>");
-            _indexHTML.AppendLine("</div>");
+            Builder.AppendLine(@"</ul>");
+            Builder.AppendLine("</div>");
         }
 
         public void StartChildren()
         {
-            _indexHTML.AppendLine(@"<div class='children'>");
-            _indexHTML.AppendLine("<h2>Child Tests</h2>");
-            _indexHTML.AppendLine(@"<ul class='childLinks'>");
+            Builder.AppendLine(@"<div class='children'>");
+            Builder.AppendLine("<h2>Child Tests</h2>");
+            Builder.AppendLine(@"<ul class='childLinks'>");
         }
 
         public void EndChildren()
         {
-            _indexHTML.AppendLine(@"</ul>");
-            _indexHTML.AppendLine("</div>");
+            Builder.AppendLine(@"</ul>");
+            Builder.AppendLine("</div>");
         }
 
         public void EndIndex()
         {
-            _indexHTML.AppendLine(@"</body>");
-            _indexHTML.AppendLine(@"</HTML>");
+            Builder.AppendLine(@"</body>");
+            Builder.AppendLine(@"</HTML>");
         }
 
         public void AddLink(string url, string description, string cssClass)
         {
-            _indexHTML.Append(@"<li><a href='");
-            _indexHTML.Append(url);
-            _indexHTML.Append("' class='");
-            _indexHTML.Append(cssClass);
-            _indexHTML.Append(@"'>");
-            _indexHTML.Append(description);
-            _indexHTML.Append(@"</a></li>");
+            Builder.Append(@"<li><a href='");
+            Builder.Append(url);
+            Builder.Append("' class='");
+            Builder.Append(cssClass);
+            Builder.Append(@"'>");
+            Builder.Append(description);
+            Builder.Append(@"</a></li>");
         }
     }
 }
