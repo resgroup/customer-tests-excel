96d9effb21b2730b675dd40493d0395989c1b472
Cedd Burge <cedd.burge@res-group.com>
2018-08-07 08:24:53 +0100

    Correct namespace

--- SampleTests/IgnoreOnGeneration/TestItineraryLeg.cs
+++ SampleTests/IgnoreOnGeneration/TestItineraryLeg.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SampleTestsRerouting
+namespace SampleTests
 {
     public class SpecificationSpecificItineraryLeg : ReportsSpecificationSetupIBlank
     {
