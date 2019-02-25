10f117fec6556eadb8d0e48064af063df42ffd1f
Cedd Burge <cedd.burge@res-group.com>
2019-02-25 18:36:26 +0000

    Add more detail to the assertions and use named ranges in excel

--- SampleSystemUnderTest/VermeulenNearWakeLength/IVermeulenNearWakeLength.cs
+++ SampleSystemUnderTest/VermeulenNearWakeLength/IVermeulenNearWakeLength.cs
@@ -3,5 +3,14 @@
     public interface IVermeulenNearWakeLength
     {
         double VermeulenNearWakeLength_m { get; }
+        double AngularVelocity { get; }
+        double TipSpeedRatio { get; }
+        double FlowFieldRatio { get; }
+        double ShearTurbulenceWakeErosionRate { get; }
+        double AmbientTurbulenceWakeErosionRate { get; }
+        double MechanicalWakeErosionRate { get; }
+        double TotalErosionRate { get; }
+        double RadiusOfInviscidExpandedRotorDisk { get; }
+        double N { get; }
     }
 }
--- SampleSystemUnderTest/VermeulenNearWakeLength/VermeulenNearWakeLengthCalculator.cs
+++ SampleSystemUnderTest/VermeulenNearWakeLength/VermeulenNearWakeLengthCalculator.cs
@@ -19,31 +19,68 @@ namespace SampleSystemUnderTest.VermeulenNearWakeLength
 
         IVermeulenNearWakeLength CalculateVermeulenNearWakeLength(IVermeulenNearWakeLengthInput input)
         {
-            // used named ranges and then copy and paste the 
+            // I have copied the formulas from Excel, which helps a lot when writing the calculation.
+            // A lot of the formulas used named ranges, but this only to works when the named 
+            // ranges are on the same row.
+            // The idea could be expanded by referencing the data in the setup in the assertion rows,
+            // and applying named ranges there, at which point everything could become a named range,
+            // using the domain language.
+            // You could also define the Excel Functions (PI(), SQRT(), IF() etc in C#, and at that
+            // point you could just copy and paste the excel formulas in to the code and they would
+            // work.
+
             double radius = input.TurbineGeometry.Diameter_m / 2;
-            double angularVelocity = input.RevolutionsPerMinute * (2 * PI / 60); // =F11* (2 * PI() / 60)
-            double tipSpeedRatio = (radius * angularVelocity) / input.Velocity_mps; // = (G28 * F28) / D11
+
+            // =F11* (2 * PI() / 60)
+            double angularVelocity = input.RevolutionsPerMinute * (2 * PI / 60); 
+
+            //=(AngularVelocity * Radius) / D11
+            double tipSpeedRatio = (radius * angularVelocity) / input.Velocity_mps;
+
+            //=IF(G11>0.8888,3,1/SQRT(1-G11))
             double flowFieldRatio = (input.ThrustCoefficient > 0.8888)
                 ? 3
-                : 1 / (Sqrt(1 - input.ThrustCoefficient)); //=IF(G11>0.8888,3,1/SQRT(1-G11))
-            double shearTurbulenceWakeErosionRate = (1 - flowFieldRatio) * Sqrt(1.49 + flowFieldRatio) / (9.76 * (1 + flowFieldRatio)); //=(1-I28)*SQRT(1.49+I28)/(9.76*(1+I28))
+                : 1 / (Sqrt(1 - input.ThrustCoefficient));
+
+            //=(1-FlowFieldRatio)*SQRT(1.49+FlowFieldRatio)/(9.76*(1+FlowFieldRatio))
+            double shearTurbulenceWakeErosionRate = (1 - flowFieldRatio) * Sqrt(1.49 + flowFieldRatio) / (9.76 * (1 + flowFieldRatio)); 
+
+            //=IF(E11>0.02,2.5*E11+0.05,5*E11)
             double ambientTurbulenceWakeErosionRate = (input.AmbientTurbuluence > 0.02)
                 ? 2.5 * input.AmbientTurbuluence + 0.05
-                : 5 * input.AmbientTurbuluence; //=IF(E11>0.02,2.5*E11+0.05,5*E11)
-            double mechanicalWakeErosionRate = 0.012 * input.TurbineGeometry.NumberOfBlades * tipSpeedRatio;//=0.012*H11*H28
-            double totalErosionRate = Sqrt(Pow(shearTurbulenceWakeErosionRate, 2) + Pow(ambientTurbulenceWakeErosionRate, 2) + Pow(mechanicalWakeErosionRate, 2));// =SQRT(POWER(J28,2)+POWER(K28,2)+POWER(L28,2))
-            double radiusOfInviscidExpandedRotorDisk = radius * Sqrt((flowFieldRatio + 1) / 2); //=F28*SQRT((I28+1)/2)
-            //=SQRT(0.214+0.144*I28) * (1-SQRT(0.134+0.124*I28))/((1-SQRT(0.214+0.144*I28))*SQRT(0.134+0.124*I28))
+                : 5 * input.AmbientTurbuluence; 
+
+            //=0.012*H11*TipSpeedRatio
+            double mechanicalWakeErosionRate = 0.012 * input.TurbineGeometry.NumberOfBlades * tipSpeedRatio;
+
+            //=SQRT(POWER(ShearTurbulenceWakeErosionRate,2)+POWER(AmbientTurbulenceWakeErosionRate,2)+POWER(MechanicalWakeErosionRate,2))
+            double totalErosionRate = Sqrt(Pow(shearTurbulenceWakeErosionRate, 2) + Pow(ambientTurbulenceWakeErosionRate, 2) + Pow(mechanicalWakeErosionRate, 2));
+
+            //=Radius*SQRT((FlowFieldRatio+1)/2)
+            double radiusOfInviscidExpandedRotorDisk = radius * Sqrt((flowFieldRatio + 1) / 2); 
+
+            //=SQRT(0.214+0.144*FlowFieldRatio)* (1-SQRT(0.134+0.124*FlowFieldRatio))/((1-SQRT(0.214+0.144*FlowFieldRatio))*SQRT(0.134+0.124*FlowFieldRatio))
             double n =
                 Sqrt(0.214 + 0.144 * flowFieldRatio) *
                 (1 - Sqrt(0.134 + 0.124 * flowFieldRatio))
                 /
                 ((1 - Sqrt(0.214 + 0.144 * flowFieldRatio)) *
                 Sqrt(0.134 + 0.124 * flowFieldRatio));
-            double vermeulenNearWakeLength_m = n * radiusOfInviscidExpandedRotorDisk / totalErosionRate;//=O28 * N28 / M28
+            
+            //=N*RadiusOfInviscidExpandedRotorDisk/TotalErosionRate
+            double vermeulenNearWakeLength_m = n * radiusOfInviscidExpandedRotorDisk / totalErosionRate;
 
             return new VermeulenNearWakeLength(
-                vermeulenNearWakeLength_m: vermeulenNearWakeLength_m
+                vermeulenNearWakeLength_m: vermeulenNearWakeLength_m,
+                angularVelocity: angularVelocity,
+                tipSpeedRatio: tipSpeedRatio,
+                flowFieldRatio: flowFieldRatio,
+                shearTurbulenceWakeErosionRate: shearTurbulenceWakeErosionRate,
+                ambientTurbulenceWakeErosionRate: ambientTurbulenceWakeErosionRate,
+                mechanicalWakeErosionRate: mechanicalWakeErosionRate,
+                totalErosionRate: totalErosionRate,
+                radiusOfInviscidExpandedRotorDisk: radiusOfInviscidExpandedRotorDisk,
+                n: n
             );
         }
     }
