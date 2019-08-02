7d098396777cb9f87372d9623da2ab7c8b4c73d7
Cedd Burge <cedd.burge@res-group.com>
2019-08-02 08:37:35 +0100

    Test underscore - space conversion

--- Test/ListPropertiesOutputWriter.cs
+++ Test/ListPropertiesOutputWriter.cs
@@ -9,7 +9,9 @@ namespace CustomerTestsExcel.Test
     [TestFixture]
     public class ListPropertiesOutputWriter : TestBase
     {
-        const string LIST_PROPERTY_NAME = "groups list of";
+        const string LIST_PROPERTY_CSHARP_NAME = "groups_list_of";
+        const string LIST_PROPERTY_EXCEL_NAME = "groups list of";
+        const string LIST_PROPERTY_HUMAN_READABLE_NAME = "groups list of";
         const string LIST_PROPERTY_TYPE = "AnovaGroup";
         const string PROPERTY1_NAME = "School";
         const string PROPERTY1_VALUE1 = "Langley";
@@ -33,7 +35,7 @@ namespace CustomerTestsExcel.Test
             };
 
             var givenListProperty = new ReportSpecificationSetupList(
-                LIST_PROPERTY_NAME,
+                LIST_PROPERTY_CSHARP_NAME,
                 LIST_PROPERTY_TYPE,
                 listPropertyItems);
 
@@ -52,10 +54,10 @@ namespace CustomerTestsExcel.Test
 
             var page = testTabularLibrary.Books[0].Pages[0];
 
-            CollectionAssert.Contains(page.SetCells.Values, LIST_PROPERTY_NAME);
+            CollectionAssert.Contains(page.SetCells.Values, LIST_PROPERTY_EXCEL_NAME);
 
             var expectedCells = Table(
-                Row(LIST_PROPERTY_NAME, LIST_PROPERTY_TYPE),
+                Row(LIST_PROPERTY_EXCEL_NAME, LIST_PROPERTY_TYPE),
                 Row(null, "With Item"),
                 Row(null, null, PROPERTY1_NAME, $"\"{PROPERTY1_VALUE1}\""),
                 Row(null, null, PROPERTY2_NAME, PROPERTY2_VALUE1),
@@ -80,7 +82,7 @@ namespace CustomerTestsExcel.Test
             // exist and have certain attributes etc. This is fine for now though, I might even remove the html
             // output at some point as it doesn't seem to be used much.
             StringAssert.Contains($"<div class='givenListProperty'>", htmlTestOutputWriter.Html);
-            StringAssert.Contains($@"<span class='propertyName'>{LIST_PROPERTY_NAME}</span>", htmlTestOutputWriter.Html);
+            StringAssert.Contains($@"<span class='propertyName'>{LIST_PROPERTY_CSHARP_NAME}</span>", htmlTestOutputWriter.Html);
             StringAssert.Contains($@"<span class='propertyType code'>{LIST_PROPERTY_TYPE}</span>", htmlTestOutputWriter.Html);
             StringAssert.Contains(@"<div class='withItem'>", htmlTestOutputWriter.Html);
             StringAssert.Contains(@"<div>With Item</div>", htmlTestOutputWriter.Html);
@@ -95,12 +97,13 @@ namespace CustomerTestsExcel.Test
         {
             var runSpecification = new RunSpecification<TestReportsSpecificationSetup>();
 
+            // This uses a StringOutputWriter if nothing else is passed in
             runSpecification.Run(new TestSpecification(setupWithListProperty));
 
             // This doesn't test everything is in the right order and things like that,
             // but it is more flexible in allowing whitespace changes, so probably an
             // ok compromise.
-            StringAssert.Contains($"{LIST_PROPERTY_NAME} list of {LIST_PROPERTY_TYPE}", runSpecification.Message);
+            StringAssert.Contains($"{LIST_PROPERTY_HUMAN_READABLE_NAME} {LIST_PROPERTY_TYPE}", runSpecification.Message);
             StringAssert.Contains("With Item", runSpecification.Message);
             StringAssert.Contains($"{PROPERTY1_NAME} \"{PROPERTY1_VALUE1}\"", runSpecification.Message);
             StringAssert.Contains($"{PROPERTY2_NAME} {PROPERTY2_VALUE1}", runSpecification.Message);
