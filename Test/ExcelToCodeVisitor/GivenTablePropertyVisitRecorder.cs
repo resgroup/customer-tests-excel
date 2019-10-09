731ec8eb4d356039c63661f8a5e5d80778507643
Cedd Burge <cedd.burge@res-group.com>
2019-10-09 09:07:35 +0100

    Add test for visiting table properties
    The test looks pretty good now, but doesn't pass. The output of the
    table headers needs to be improved, and then ExcelToCode needs to
    call the visitor in the way that the test specifies

---
+++ Test/ExcelToCodeVisitor/GivenTablePropertyVisitRecorder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomerTestsExcel.ExcelToCode;
+using NUnit.Framework;
+
+namespace CustomerTestsExcel.Test.ExcelToCodeVisitor
+{
+    public class GivenTablePropertyVisitRecorder : IExcelToCodeVisitor
+    {
+        readonly List<string> recordedTableProperties = new List<string>();
+        public IReadOnlyList<string> RecordedTableProperties =>
+            recordedTableProperties;
+
+        public void VisitGivenTablePropertyDeclaration(IEnumerable<TableHeader> tableHeaders) =>
+            recordedTableProperties.Add($"Table {tableHeaders}");
+
+        public void VisitGivenTablePropertyRowDeclaration(uint row) =>
+            recordedTableProperties.Add($"RowDeclaration {row}");
+
+        public void VisitGivenTablePropertyCellDeclaration(TableHeader tableHeader, uint row, uint column) =>
+            recordedTableProperties.Add($"Cell({row}, {column}) {tableHeader}");
+
+        public void VisitGivenComplexPropertyDeclaration(IGivenComplexProperty givenComplexProperty) =>
+            recordedTableProperties.Add(givenComplexProperty.ToString());
+
+        public void VisitGivenComplexPropertyFinalisation() =>
+            recordedTableProperties.Add("ComplexPropertyFinalisation");
+
+        public void VisitGivenSimpleProperty(IGivenSimpleProperty givenSimpleProperty) =>
+            recordedTableProperties.Add(givenSimpleProperty.ToString());
+
+        public void VisitGivenTablePropertyCellFinalisation() =>
+            recordedTableProperties.Add("CellFinalisation");
+
+        public void VisitGivenTablePropertyRowFinalisation() =>
+            recordedTableProperties.Add("RowFinalisation");
+
+        public void VisitGivenTablePropertyFinalisation() =>
+            recordedTableProperties.Add("TableFinalisation");
+
+        public void VisitGivenListPropertyDeclaration(IGivenListProperty givenListProperty)
+        {
+            // ignore to keep test simple, just focus on the complex visits
+        }
+
+        public void VisitGivenListPropertyFinalisation()
+        {
+            // ignore to keep test simple, just focus on the complex visits
+        }
+    }
+}
---
+++ Test/ExcelToCodeVisitor/VisitGivenTablePropertes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using CustomerTestsExcel.ExcelToCode;
+using NUnit.Framework;
+
+namespace CustomerTestsExcel.Test.ExcelToCodeVisitor
+{
+    [TestFixture]
+    public class VisitGivenTablePropertes : TestBase
+    {
+        [Test]
+        public void ExcelToCodeVisitsTableProperties()
+        {
+            var visitRecorder = new GivenTablePropertyVisitRecorder();
+
+            // The GivenTablePropertyVisitRecorder outputs strings for the various visits encountered,
+            // The values aren't exactly the same as the ones in the spreadsheet, as they are formatted by both
+            // ExcelToCode and CodeNameToExcelNameConverter in to the strings that are used in the code.
+            // This is a shame, it would be good to make this more loosely coupled, so that we could use a 
+            // test formatter, and make the assertions exactly the same as the excel.
+            var expected = new List<string>
+            {
+                "thingToSetup, SpecificationSpecificThingToSetup",
+                    // need to fix headers.tostring() to make it look better here
+                    "Table System.Collections.Generic.Dictionary`2+ValueCollection[System.UInt32,CustomerTestsExcel.ExcelToCode.TableHeader]",
+                        "RowDeclaration 0",
+                            "Cell(0, 0) CustomerTestsExcel.ExcelToCode.SubClassTableHeader",
+                                "complexPropertyOf, SpecificationSpecificComplexClass",
+                                    "ComplexProperty1_of, \"ComplexProperty1Value1\", String",
+                                    "ComplexProperty2_of, \"ComplexProperty2Value1\", String",
+                                "ComplexPropertyFinalisation",
+                            "CellFinalisation",
+                            "Cell(0, 2) CustomerTestsExcel.ExcelToCode.PropertyTableHeader",
+                                "SimpleProperty_of, \"SimplePropertyValue1\", String",
+                            "CellFinalisation",
+                        "RowFinalisation",
+                        "RowDeclaration 1",
+                            "Cell(1, 0) CustomerTestsExcel.ExcelToCode.SubClassTableHeader",
+                                "complexPropertyOf, SpecificationSpecificComplexClass",
+                                    "ComplexProperty1_of, \"ComplexProperty1Value2\", String",
+                                    "ComplexProperty2_of, \"ComplexProperty2Value2\", String",
+                                "ComplexPropertyFinalisation",
+                            "CellFinalisation",
+                            "Cell(1, 2) CustomerTestsExcel.ExcelToCode.PropertyTableHeader",
+                                "SimpleProperty_of, \"SimplePropertyValue2\", String",
+                            "CellFinalisation",
+                        "RowFinalisation",
+                    "TableFinalisation",
+                "ComplexPropertyFinalisation"
+            };
+
+            var sheetConverter = new ExcelToCode.ExcelToCode(new CodeNameToExcelNameConverter(ANY_STRING));
+            sheetConverter.AddVisitor(visitRecorder);
+
+            using (var workbook = Workbook(@"TestExcelFiles\VisitGivenTableProperties.xlsx"))
+            {
+                sheetConverter.GenerateCSharpTestCode(NO_USINGS, workbook.GetPage(0), ANY_ROOT_NAMESPACE, ANY_WORKBOOKNAME);
+
+                Assert.AreEqual("", string.Join("\",\n\"", visitRecorder.RecordedTableProperties));
+                CollectionAssert.AreEqual(expected, visitRecorder.RecordedTableProperties);
+            }
+        }
+    }
+}
