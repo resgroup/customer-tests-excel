efa08eb27d6ed97b26fcb6cd8abd27d2b8f1c6da
ceddlyburge <ceddlyburge@gmail.com>
2020-04-08 20:31:30 +0100

    Avoid compile errors when no setup classes are generated
    If no classes get generated, the using statement will cause a crash
    (as there are no files that define the namespace).
    
    This done by creating a dummy file, with the namespace, and it doubles
    as an explanation of how the setup files are generated.

--- ExcelToCode/TestProjectCreator.cs
+++ ExcelToCode/TestProjectCreator.cs
@@ -56,6 +56,68 @@ namespace CustomerTestsExcel.ExcelToCode
                 OutputWorkbook(specificationFolder, projectRootNamespace, usings, assertionClassPrefix, excel, logger, compileItemGroupNode, excelFileName);
             }
 
+            GenerateSpecificationSpecificSetupClasses(
+                specificationFolder, 
+                projectRootNamespace, 
+                usings, 
+                assembliesUnderTest, 
+                project, 
+                compileItemGroupNode);
+
+            var projectRelativePath = Path.Combine("GeneratedSpecificationSpecific", "Placeholder.cs");
+            var outputPath = Path.Combine(specificationFolder, projectRelativePath);
+            Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+
+            File.WriteAllText(outputPath, $@"namespace {projectRootNamespace}.GeneratedSpecificationSpecific
+{{
+    // This folder / namespace contains all the generated specification specific setup files
+
+    // Files are generated unless there is already a matching '<ClassName>.cs' file in the
+    // IgnoreUnderGeneration folder. This allows you to override the generated files if you 
+    // want to.
+
+    // A file is generated for each root obect being tested. Only the setup parts of the
+    // class is generated, but the classes are `partial`, so you can add the rest of the 
+    // implementation in the IgnoreUnderGeneration folder. The annoyance is that you have
+    // to use this namespace, even though the file is in a different folder.
+    // An example of this is:
+    // https://github.com/resgroup/customer-tests-excel/blob/master/SampleTests/GeneratedSpecificationSpecific/Calculator.cs
+
+    // The `/assembliesUnderTest` specified on the command line are searched to find matching
+    // interfaces, and suitable files generated for them. 
+    // An example of this is:
+    // https://github.com/resgroup/customer-tests-excel/blob/master/SampleTests/GeneratedSpecificationSpecific/Cargo.cs
+    // If there is no matching interface then a dummy file is created. This will compile, but 
+    // won't set anything up in your code. These files can be useful for showing how things 
+    // work, and sometimes you might just to just use them, and map values from them in to your 
+    // system under test manually in the root object files. 
+    // An example of this is:
+    // https://github.com/resgroup/customer-tests-excel/blob/master/SampleTests/GeneratedSpecificationSpecific/A_Table.cs
+}}");
+
+            compileItemGroupNode.Add(
+                new XElement(
+                    XName.Get(
+                        "Compile",
+                        compileItemGroupNode.Name.Namespace.NamespaceName
+                        ),
+                new XAttribute("Include", projectRelativePath)
+                )
+            );
+
+            SaveProjectFile(projectFilePath, project);
+
+            return success ? 0 : 1;
+        }
+
+        private void GenerateSpecificationSpecificSetupClasses(
+            string specificationFolder, 
+            string projectRootNamespace, 
+            IEnumerable<string> usings, 
+            IEnumerable<string> assembliesUnderTest, 
+            XDocument project, 
+            XElement compileItemGroupNode)
+        {
             var assemblyTypes = new List<Type>();
 
             foreach (var assemblyFilename in assembliesUnderTest)
@@ -123,10 +185,6 @@ namespace CustomerTestsExcel.ExcelToCode
                     }
                 }
             );
-
-            SaveProjectFile(projectFilePath, project);
-
-            return success ? 0 : 1;
         }
 
         IEnumerable<string> ListValidSpecificationSpreadsheets(string excelFolder)
