6be94970aa16dc8b19346e8dcff3aeef9a0ecb4e
ceddlyburge <ceddlyburge@gmail.com>
2020-07-28 19:22:27 +0100

    Update generatetests batch script now that it has changed location

--- GenerateTests.bat
+++ GenerateTests.bat
@@ -1,4 +1,4 @@
 rem calls the real generatetest file, but from the directory it is expecting to be in, this makes the appveyor build a bit easier
-pushd .\SampleTests\IgnoreOnGeneration
+pushd .\SampleTests
 call GenerateTests.bat
 popd
